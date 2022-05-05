63f08f21be3895cad6461318a088430058a05c7d
kiasaki <kiasaki0000@gmail.com>
2022-05-05 11:18:58 -0400

    sql queries

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module wocto
 
 go 1.18
+
+require github.com/mattn/go-sqlite3 v1.14.12
--- main.go
+++ main.go
@@ -1,17 +1,67 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
+var db *sql.DB
+
 func main() {
+	var err error
+	db, err = sql.Open("sqlite3", "data.sqlite3")
+	check(err)
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS projects (id primary key, name, slug, user_id);`)
+	check(err)
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS pages (id primary key, name, project_id, content);`)
+	check(err)
+	_, err = db.Exec(`INSERT INTO projects VALUES ('1', 'Wocto', '', '1') ON CONFLICT (id) DO NOTHING;`)
+	check(err)
 	log.Println("Starting on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", http.HandlerFunc(handler)))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	a, err := dbRows(`SELECT * FROM projects`)
+	check(err)
+	fmt.Fprintf(w, "Hello, %#v", a)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func dbRows(sql string, args ...interface{}) ([]map[string]interface{}, error) {
+	r, err := db.Query(sql, args...)
+	if err != nil {
+		return nil, err
+	}
+	results := []map[string]interface{}{}
+	columns, err := r.Columns()
+	if err != nil {
+		return nil, err
+	}
+	for r.Next() {
+		values := make([]interface{}, len(columns))
+		for i := range values {
+			values[i] = new(interface{})
+		}
+		err := r.Scan(values...)
+		if err != nil {
+			return nil, err
+		}
+
+		result := map[string]interface{}{}
+		for i, column := range columns {
+			result[column] = *(values[i].(*interface{}))
+		}
+		results = append(results, result)
+	}
+	return results, r.Err()
 }
